601b26018249a93693506a15c61063b351584b18
Miguel <miguelalba89@gmail.com>
2019-09-18 13:43:34 +0200

    Remove old code

--- src/main/java/de/bund/bfr/knime/fsklab/nodeengine/java2node/Swagger2JSONFormsBridge.java
+++ src/main/java/de/bund/bfr/knime/fsklab/nodeengine/java2node/Swagger2JSONFormsBridge.java
@@ -1,8 +1,21 @@
 package de.bund.bfr.knime.fsklab.nodeengine.java2node;
 
-import java.io.BufferedWriter;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.util.Yaml;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,27 +24,6 @@ import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Stream;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
-import io.swagger.util.Yaml;
-
-import javax.json.JsonException;
 
 public class Swagger2JSONFormsBridge {
     static Map<String, String> filesAndContent = new Hashtable<String, String>();
@@ -47,7 +39,6 @@ public class Swagger2JSONFormsBridge {
     public static void main(String[] args) {
 
         Swagger swagger = new SwaggerParser().read("./model.yaml");
-        //File jsonFile = new File("./swagger.json");
 
         try {
             FileUtils.cleanDirectory(new File("./generatedswaggersubfiles"));
@@ -57,16 +48,8 @@ public class Swagger2JSONFormsBridge {
 
             String contents = convertYamlToJson(yaml);
 
-            // Ahmad
-//            JSONObject jObject = new JSONObject(contents).getJSONObject("definitions");
-//            // First step to extract JSON Object into Files
-//            iteratOverJSONObject("", "Root", jObject, false, "");
-            // -----------------------------------------------------------------
-
-            // Miguel
             ObjectNode jObject2 = (ObjectNode) Yaml.mapper().readTree(contents).get("definitions");
             iterateOverJSONObject("", "Root", jObject2, false, "");
-            // -----------------------------------------------------------------
 
             // Make in-Memory map to keep the Objects for later
             try (Stream<Path> walk = Files.walk(Paths.get("./generatedswaggersubfiles"))) {
@@ -93,19 +76,6 @@ public class Swagger2JSONFormsBridge {
                 walk.forEach(path -> {
                     if (path.toFile().isFile()) {
                         try {
-
-                            // AHMAD ...
-//                            String firstGeneratedContent = new String(Files.readAllBytes(path));
-//                            JSONObject changedJSONObject = iteratOverGeneratedFilesAndReplaceReferences(
-//                                    new JSONObject(firstGeneratedContent), path.toString());
-//                            // System.out.println(path.getFileName().toString());
-//
-//                            String fileNameWithOutExt = FilenameUtils.removeExtension(path.getFileName().toString());
-//                            File f = new File("./generatedswaggermainfiles", fileNameWithOutExt + "Model.json");
-//                            saveObjectDefintion(changedJSONObject, f);
-                            // .........................................................................................
-
-                            // MIGUEL
                             // TODO: The file could be read directly with the mapper but order could lost. Check later.
                             String firstGeneratedContent = FileUtils.readFileToString(path.toFile());
                             JsonNode changedJsonObject = iterateOverGeneratedFilesAndReplaceReferences(Yaml.mapper().readTree(firstGeneratedContent), path.toString());
@@ -113,7 +83,6 @@ public class Swagger2JSONFormsBridge {
                             String fileNameWithOutExt = FilenameUtils.removeExtension(path.getFileName().toString());
                             File f = new File("./generatedswaggermainfiles", fileNameWithOutExt + "Model.json");
                             FileUtils.writeStringToFile(f, changedJsonObject.toString());
-                            // .........................................................................................
 
                         } catch (IOException e) {
                             // TODO Auto-generated catch block
@@ -133,22 +102,6 @@ public class Swagger2JSONFormsBridge {
                 walk.forEach(path -> {
                     if (path.toFile().isFile()) {
                         try {
-                            // AHMAD
-//                            String firstGeneratedContent = new String(Files.readAllBytes(path));
-//                            System.out.println(path.getFileName().toString());
-//                            JSONObject originalJSONObject = new JSONObject(firstGeneratedContent);
-//                            if (originalJSONObject.has("properties")) {
-//                                JSONObject changedJSONObject = generateUISchema(
-//                                        originalJSONObject.getJSONObject("properties"), path.toString());
-//                                String fileNameWithOutExt = FilenameUtils
-//                                        .removeExtension(path.getFileName().toString());
-//                                File f = new File("./generatedswaggermainfiles",
-//                                        replaceLast(fileNameWithOutExt, "Model", "") + "View.json");
-//                                saveObjectDefintion(changedJSONObject, f);
-//                            }
-                            // .........................................................................................
-
-                            // MIGUEL
                             String firstGeneratedContent = FileUtils.readFileToString(path.toFile());
                             JsonNode originalJsonObject = Yaml.mapper().readTree(firstGeneratedContent);
                             if (originalJsonObject.has("properties")) {
@@ -159,7 +112,6 @@ public class Swagger2JSONFormsBridge {
                                         replaceLast(fileNameWithOutExt, "Model", "") + "View.json");
                                 FileUtils.writeStringToFile(f, changedJsonObject.toString());
                             }
-                            // .........................................................................................
                         } catch (Exception e) {
                             System.out.println(path.getFileName().toString());
                             e.printStackTrace();
@@ -172,70 +124,6 @@ public class Swagger2JSONFormsBridge {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void iteratOverJSONObject(String tabbing, String objectName, JSONObject jObject,
-                                            boolean keepObjectName, String parent) throws JSONException, IOException {
-        if (jObject.has("items") && ((String) jObject.get("type")).equals("array")
-                && ((JSONObject) jObject.get("items")).has("type")
-                && ((JSONObject) jObject.get("items")).get("type").equals("string")) {
-            JSONObject propertiesObject = new JSONObject();
-            propertiesObject.put("value", jObject.get("items"));
-            jObject.remove("items");
-            JSONObject itemsObject = new JSONObject();
-            itemsObject.put("properties", propertiesObject);
-            itemsObject.put("type", "object");
-            itemsObject.put("additionalProperties", false);
-            jObject.put("items", itemsObject);
-            // System.out.println(jObject);
-        }
-        if (jObject.has("properties") && jObject.has("type") && ((String) jObject.get("type")).equals("object")) {
-            jObject.put("additionalProperties", false);
-        }
-
-        Iterator<String> keys = jObject.keys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            // System.out.println(tabbing + objectName + " has a key " + key);
-
-            if (jObject.get(key) instanceof JSONObject) {
-                iteratOverJSONObject(tabbing + "\t", keepObjectName ? objectName : key, jObject.getJSONObject(key),
-                        false, parent);
-                if (keepObjectName) {
-                    // saveObjectDefintion(jObject.getJSONObject(key),
-                    // new File("./generatedswaggermainfiles", objectName + ".json"));
-                } else {
-                    if (!jObject.getJSONObject(key).toString().startsWith("{\"$ref\":\"#")) {
-                        if (!objectName.equals("items") && jObject.has("type")
-                                && jObject.get("type").equals("object")) {
-
-                            if (!StringUtils.isEmpty(parent)) {
-                                saveObjectDefintion(jObject,
-                                        new File("./generatedswaggersubfiles",
-                                                parent + "_" + objectName.substring(0, 1).toLowerCase()
-                                                        + objectName.substring(1, objectName.length()) + ".json"));
-
-                            } else {
-                                saveObjectDefintion(jObject,
-                                        new File("./generatedswaggersubfiles", objectName + ".json"));
-                            }
-
-                        }
-                    } else {
-                        if (!StringUtils.isEmpty(parent)) {
-                            File file = new File("./generatedswaggersubfiles", (parent != "" ? parent + "_" : "")
-                                    + key.substring(0, 1).toLowerCase() + key.substring(1, key.length()) + ".json");
-                            if (!file.exists()) {
-                                saveObjectDefintion(jObject.getJSONObject(key), file);
-                            }
-                        }
-
-                    }
-                }
-            } else if (jObject.get(key) instanceof JSONArray && key.equals("allOf")) {
-                iteratOverJSONArray(tabbing + "\t", objectName, jObject.getJSONArray(key), objectName);
-            }
         }
     }
 
@@ -276,7 +164,6 @@ public class Swagger2JSONFormsBridge {
                         if (!objectName.equals("items") &&
                                 jObject.has("type") && jObject.get("type").asText().equals("object")) {
 
-                            // Skip model definitions. A model has its model name as parent and objectName (like GenericModel)
                             if (!StringUtils.isEmpty(parent)) {
                                 File definitionFile = new File("./generatedswaggersubfiles",
                                         parent + "_" + objectName.substring(0, 1).toLowerCase()
@@ -301,24 +188,6 @@ public class Swagger2JSONFormsBridge {
         }
     }
 
-    public static JSONObject generateUISchema(JSONObject jObject, String fileName) throws JSONException, IOException {
-        Iterator<String> keys = jObject.keys();
-        JSONObject JSONUISchema = new JSONObject();
-        JSONUISchema.put("type", "VerticalLayout");
-        JSONArray elementsArray = new JSONArray();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            // System.out.println(fileName + " has a key " + key);
-            JSONObject JSONUISchemaItem = new JSONObject();
-            JSONUISchemaItem.put("type", "Control");
-            JSONUISchemaItem.put("label", capitaliseFirstLetter(splitCamelCase(key)));
-            JSONUISchemaItem.put("scope", "#/properties/" + key);
-            elementsArray.put(JSONUISchemaItem);
-        }
-        JSONUISchema.put("elements", elementsArray);
-        return JSONUISchema;
-    }
-
     public static ObjectNode generateUISchema(ObjectNode jObject, String filename) {
 
         Iterator<String> keys = jObject.fieldNames();
@@ -338,43 +207,6 @@ public class Swagger2JSONFormsBridge {
         schema.put("type", "VerticalLayout");
         schema.set("elements", elementsArray);
         return schema;
-    }
-
-    public static JSONObject iteratOverGeneratedFilesAndReplaceReferences(JSONObject jObject, String fileName)
-            throws JSONException, IOException {
-        Iterator<String> keys = jObject.keys();
-        JSONObject editedOne = new JSONObject(jObject, JSONObject.getNames(jObject));
-
-        while (keys.hasNext()) {
-            String key = keys.next();
-
-            if (jObject.get(key) instanceof JSONObject) {
-                if (((JSONObject) jObject.get(key)).has("$ref")) {
-                    // System.out.println(fileName + " " + ((JSONObject)
-                    // jObject.get(key)).get("$ref"));
-                    String ref = (String) ((JSONObject) jObject.get(key)).get("$ref");
-                    String replacerFileName = ".\\generatedswaggersubfiles\\"
-                            + ref.split("/")[ref.split("/").length - 1] + ".json";
-                    String contents = filesAndContent.get(replacerFileName);
-                    JSONObject replacerJObject = new JSONObject(contents);
-                    editedOne.remove(key);
-                    editedOne.put(key, replacerJObject);
-                }
-                JSONObject subJObject = editedOne.getJSONObject(key);
-                subJObject = iteratOverGeneratedFilesAndReplaceReferences(subJObject, fileName);
-                editedOne.put(key, subJObject);
-            } else if (key.equals("$ref")) {
-                System.out.println(jObject);
-                String ref = (String) jObject.get(key);
-                String replacerFileName = ".\\generatedswaggersubfiles\\" + ref.split("/")[ref.split("/").length - 1]
-                        + ".json";
-                String contents = filesAndContent.get(replacerFileName);
-                JSONObject replacerJObject = new JSONObject(contents);
-                replacerJObject = iteratOverGeneratedFilesAndReplaceReferences(replacerJObject, fileName);
-                editedOne = replacerJObject;
-            }
-        }
-        return editedOne;
     }
 
     private static JsonNode iterateOverGeneratedFilesAndReplaceReferences(JsonNode jObject, String filename) throws IOException {
@@ -413,20 +245,6 @@ public class Swagger2JSONFormsBridge {
         return editedOne;
     }
 
-    public static void iteratOverJSONArray(String tabbing, String objectName, JSONArray jArray, String parent) {
-        jArray.forEach(item -> {
-            if (item instanceof JSONObject) {
-                JSONObject currentJSonObject = (JSONObject) item;
-                try {
-                    iteratOverJSONObject(tabbing, objectName, currentJSonObject, true, parent);
-                } catch (JSONException | IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
     private static void iterateOverJsonArray(String tabbing, String objectName, ArrayNode jArray, String parent) {
         Iterator<JsonNode> elements = jArray.elements();
         while (elements.hasNext()) {
@@ -439,13 +257,6 @@ public class Swagger2JSONFormsBridge {
                 }
             }
         }
-    }
-
-    public static void saveObjectDefintion(JSONObject jsonDefinition, File output) throws IOException {
-        FileWriter fileWritter = new FileWriter(output.getPath(), false);
-        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-        bufferWritter.write(jsonDefinition.toString());
-        bufferWritter.close();
     }
 
     static String splitCamelCase(String s) {
